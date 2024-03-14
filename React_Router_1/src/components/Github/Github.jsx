9af3a4498eb550ee3dfdef7723c67eb286f3d377
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2024-03-14 20:58:20 +0530

    Github section responsiveness done

--- React_Router_1/src/components/Github/Github.jsx
+++ React_Router_1/src/components/Github/Github.jsx
@@ -33,21 +33,21 @@ export default function Github() {
   return (
     <>
       <Navbar />
-      <main className="flex justify-between sm:flex-row w-full bg-gray-700 sm:py-10 sm:px-4 sm:w-10/12 mx-auto">
-        <div className="w-full">
+      <main className="flex justify-between flex-col sm:flex-row max-sm:rounded-md bg-gray-700 py-5 sm:py-10 sm:px-4 w-11/12 sm:w-10/12 max-sm:mt-20 mx-auto">
+        <div className="w-full ">
           <img
-            className="sm:w-2/4 rounded-lg"
+            className="w-3/4 sm:w-2/4 rounded-full max-sm:mx-auto"
             src={githubData.profilePicture}
             alt="Profile Picture"
           />
-          <div className="w-fit text-center sm:text-2xl font-semibold text-gray-200">
+          <div className="w-fit text-center text-lg sm:text-2xl font-semibold text-gray-200 max-sm:mx-auto">
             <h2>userId:</h2>
             <h2>{githubData.userId}</h2>
           </div>
         </div>
 
-        <section className="flex flex-col justify-evenly w-full">
-          <div className="flex w-1/2 justify-between sm:text-xl font-semibold text-gray-200">
+        <section className="flex flex-col justify-evenly w-full max-sm:gap-4">
+          <div className="flex w-full sm:w-1/2 justify-evenly sm:justify-between sm:text-xl font-semibold text-gray-200">
             <div className="flex flex-col">
               <h2>Followers</h2>
               <h2 className="text-center">{githubData.followers}</h2>
@@ -58,11 +58,11 @@ export default function Github() {
             </div>
           </div>
           <div>
-            <div className="flex flex-row gap-10 sm:text-xl font-semibold text-gray-200">
+            <div className="flex max-sm:justify-evenly sm:flex-row gap-10 sm:text-xl font-semibold text-gray-200">
               <h2>Public Repositories: </h2>
               <h2>{githubData.publicRepos}</h2>
             </div>
-            <div className="flex flex-row gap-10 sm:text-xl font-semibold text-gray-200">
+            <div className="flex flex-row gap-2 sm:gap-10 text-xm sm:text-xl max-sm:justify-evenly sm:font-semibold text-gray-200">
               <h2>URL: </h2>
               <a href={githubData.url}>{githubData.url}</a>
             </div>
